a9c94974882a34d3b4f175b881ceb30dd1867be8
Andrew Crawford <112627841+acemagneto@users.noreply.github.com>
2022-09-05 16:17:13 +0100

    Add new PhotoDetail page object

---
+++ PhotosXCUITest/PhotosUITests/PhotoDetail.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoDetail.swift
+//  PhotosUITests
+//
+//  Created by Andrew Crawford on 05/09/2022.
+//
+
+import XCTest
+
+class PhotoDetail: XCTestCase {
+    private let appHelper = AppHelper()
+    private var app: XCUIApplication {appHelper.app}
+    private static let deleteButtonIdentifier = "Delete"
+    private static let confirmDeletePhotoButtonIdentifier = "Delete Photo"
+    private static let backButtonIdentifier = "All Photos"
+    
+    func deletePhoto() {
+        XCTAssert(app.buttons[PhotoDetail.deleteButtonIdentifier].waitForExistence(timeout: appHelper.timeout))
+        app.buttons[PhotoDetail.deleteButtonIdentifier].tap()
+    }
+    
+    func confirmDeletion() {
+        XCTAssert(app.buttons[PhotoDetail.confirmDeletePhotoButtonIdentifier].exists)
+        app.buttons[PhotoDetail.confirmDeletePhotoButtonIdentifier].tap()
+    }
+    
+    func navigateBackToPhotosGrid() {
+        XCTAssert(app.buttons[PhotoDetail.backButtonIdentifier].exists)
+        app.buttons[PhotoDetail.backButtonIdentifier].tap()
+    }
+}
